4eaca71dc71f44aa235fc10d742e4e2cabf75809
simonstadlinger <stadlinger.simon@gmail.com>
2021-07-19 20:32:29 +0200

    Add various configurations and basic_test configuration running

--- basic_test.py
+++ basic_test.py
@@ -1,22 +1,46 @@
-from benchmark.snap_benchmark import SnapBenchmark
 from benchmark.benchmark_factory import create_benchmarks_from_config
 import logging
-import sys
 from benchmark.benchmark_runner import BenchmarkRunner
-
-
 
 logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == "__main__":
-    logging.info("\n*************************************************\n"
-         "*                                               *\n"
-         "*             starting experiment               *\n"
-         "*                                               *\n"
-         "*************************************************\n")
+  logging.info("\n*************************************************\n"
+               "*                                               *\n"
+               "*             starting experiment               *\n"
+               "*                                               *\n"
+               "*************************************************\n")
 
-    benchmarks = create_benchmarks_from_config('configuration-email-core.yml')
+  configuration_files = [
+    'configuration-email-core-networkx.yml',
+    'configuration-email-core-graphtools.yml',
+    'configuration-email-core-snap.yml',
+    'configuration-dbpl-networkx.yml',
+    'configuration-dbpl-graphtools.yml',
+    'configuration-dbpl-snap.yml',
+    'configuration-wiki-topcats-networkx.yml',
+    'configuration-wiki-topcats-graphtools.yml',
+    'configuration-friendster-networkx.yml',
+    'configuration-friendster-graphtools.yml',
+  ]
+
+  for configuration in configuration_files:
+    logging.info("Starting to run '" + configuration + "'\n")
+    benchmarks = create_benchmarks_from_config(configuration)
     runner = BenchmarkRunner(benchmarks)
-    runner.run()
-    runner.evaluate()
-    runner.collect_results()
+    try:
+      runner.run()
+    except Exception as e:
+      logging.error("Failed to RUN '" + configuration + "': " + e)
+
+    try:
+      runner.evaluate()
+    except Exception as e:
+      logging.error("Failed to EVALUATE '" + configuration + "': " + e)
+
+    try:
+      runner.collect_results()
+    except Exception as e:
+      logging.error("Failed to COLLECT_RESULTS for '" + configuration + "': " + e)
+    logging.info("Finished to run '" + configuration + "'\n")
+
