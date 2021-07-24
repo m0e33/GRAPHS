b4f5d8abc077c1d471be1a88d861085f692aee5d
Moritz Spranger <moritz.spranger@gmail.com>
2021-07-24 15:32:35 +0200

    Fix evaluation

--- benchmark/graph_tool_benchmark.py
+++ benchmark/graph_tool_benchmark.py
@@ -6,6 +6,14 @@ import numpy as np
 from evaluation.graphtool_evaluator import GraphToolEvaluator
 from benchmark.base_benchmark import AlgorithmNotFound
 
+def multiflip_mcmc_sweep(state):
+    for i in range(1):  # this should be sufficiently large
+        state.multiflip_mcmc_sweep(beta=np.inf, niter=1)
+    return state
+
+def mcmc_anneal(state):
+    gt.mcmc_anneal(state, beta_range=(1, 10), niter=1, mcmc_equilibrate_args=dict(force_niter=10)) # from documentation
+    return state
 
 class GraphToolBenchmark(Benchmark):
     def __init__(self, config: Benchmark.Configuration):
@@ -28,9 +36,8 @@ class GraphToolBenchmark(Benchmark):
             self._logger.info(self._logger_prefix + f"Succesfully ran community detection.")
 
         elif (self._config.algorithm == "multiflip_mcmc_sweep"):
-            state = gt.BlockState(self._graph)
-            for i in range(1):  # this should be sufficiently large
-                state.multiflip_mcmc_sweep(beta=np.inf, niter=10)
+            state = gt.minimize_blockmodel_dl(self._graph)
+            self._communities = self._measure_time_and_get_results(multiflip_mcmc_sweep, state)
             self._logger.info(self._logger_prefix + f"Succesfully ran community detection.")
 
         elif (self._config.algorithm == "mcmc_anneal"):
