4f6db320eabdfeb0f6a1cdbf41e79bb54d9efa31
simonstadlinger <stadlinger.simon@gmail.com>
2021-07-26 11:55:04 +0200

    Reconfigure tmux scripts

--- tmux-script-node-10.sh
+++
@@ -1,4 +0,0 @@
-tmux new -ds networkx_k_clique 'conda activate snap-env; python basic_test.py configs-networkx/k_clique.yml True True; exec $SHELL'
-sleep 3
-tmux new -ds networkx_label_propagation 'conda activate snap-env; python basic_test.py configs-networkx/label_propagation_communities.yml True True; exec $SHELL'
-sleep 3
--- tmux-script-node-9.sh
+++ tmux-script-node-9.sh
@@ -1,4 +1,11 @@
-tmux new -ds graphtool_minimize_blockmodel_mcmc_sweep 'conda activate snap-env; python basic_test.py configs-graphtool/multiflip_mcmc_sweep.yml True True; exec $SHELL'
+tmux new -ds networkx_greedy_modularity 'conda activate snap-env; python basic_test.py configs-networkx/greedy_modularity_communities.yml False False; exec $SHELL'
 sleep 3
-tmux new -ds networkx_asyn_lpa 'conda activate snap-env; python basic_test.py configs-networkx/asyn_lpa_communities.yml True True; exec $SHELL'
+tmux new -ds networkx_k_clique 'conda activate snap-env; python basic_test.py configs-networkx/k_clique.yml False False; exec $SHELL'
 sleep 3
+tmux new -ds networkx_label_propagation 'conda activate snap-env; python basic_test.py configs-networkx/label_propagation_communities.yml False False; exec $SHELL'
+sleep 3
+tmux new -ds networkx_lukes_partitioning 'conda activate snap-env; python basic_test.py configs-networkx/lukes_partitioning.yml False False; exec $SHELL'
+sleep 3
+tmux new -ds snap_cnm 'conda activate snap_env; python basic_test.py configs-snap/CNM.yml False False; exec $SHELL'
+sleep 3
+tmux new -ds snap_girvan_newman 'conda activate snap-env; python basic_test.py configs-snap/girvan_newman.yml False False; exec $SHELL'
