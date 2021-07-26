f8c73721ea390deb1bfa6bdae2d59dd960538d5c
Moritz Spranger <moritz.spranger@gmail.com>
2021-07-26 14:27:04 +0200

    Add networkx mem tmux

--- convert_dat_to_png.py
+++ convert_dat_to_png.py
@@ -1,7 +1,19 @@
 import os
+import subprocess
+from pathlib import Path
 import sys
-path = str(sys.argv[1])
 
-for file in os.listdir(path):
-    if ".dat" in file:
-        command = ["mprof", "plot", "-f", file, "-o", f"{file}.png"]
+if __name__=="__main__":
+    path = Path(sys.argv[1])
+    path_images = path.joinpath("images")
+    if not path_images.is_dir():
+        path_images.mkdir()
+    for file in os.listdir(path):
+        if ".dat" in file:
+            file_path = path.joinpath(file)
+            new_image_path = path_images.joinpath(file)
+            if not new_image_path.is_file():
+                print(f"{path_images.joinpath(file)}")
+
+                command = ["mprof", "plot", "-f", file_path, "-o", f"{new_image_path}.png"]
+                subprocess.call(command)
