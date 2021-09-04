b7007ded08f7d3edc8c7df6a367cd044f8ded29d
Moritz Spranger <moritz.spranger@gmail.com>
2021-09-04 10:46:40 +0200

    Adapt memprofile output name

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from datetime import datetime
 
 parser = argparse.ArgumentParser(description='Run benchmark framework')
 parser.add_argument('-config', type=str, help='path to config')
-parser.add_argument('-execute_fitness', type=bool, help='Run fitness evaluation')
-parser.add_argument('-execute_partition', type=bool, help='Run partition evaluation')
-parser.add_argument('-mem_profiling', type=bool, help='Run memory profiling wth memprof')
+parser.add_argument('-execute_fitness', action='store_true', help='Run fitness evaluation')
+parser.add_argument('-execute_partition', action='store_true', help='Run partition evaluation')
+parser.add_argument('-mem_profiling', action='store_true', help='Run memory profiling wth memprof')
 
 args = parser.parse_args()
 config_path = args.config
@@ -16,7 +16,7 @@ mem_profiling = args.mem_profiling
 
 if mem_profiling:
     current_time = datetime.now().strftime("%d-%b-%Y_%H:%M:%S")
-    output_name = f"{config_path.split('/')[-1]}--{current_time}"
+    output_name = f"{current_time}--{config_path.split('/')[-1]}"
     subprocess.run(["mprof", "run", "--include-children", "--multiprocess", "--output", f"./mem_logs/{output_name}", "--python", "python3", "basic_test.py", str(config_path), str(execute_fitness), str(execute_partition)])
 else:
     subprocess.run(
