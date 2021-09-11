fd502c6bccad4f7d409aebf001b1e81368bc8bc1
Moritz Spranger <moritz.spranger@gmail.com>
2021-09-11 12:33:02 +0200

    Fix snap wiki snap dblp

--- tmux_snap_cnm.sh
+++ tmux_snap_cnm.sh
@@ -1,3 +1,3 @@
 tmux new -ds snap_email_cnm 'source /hpi/fs00/share/complexnetSS2021/mosi/anaconda3/profile.d/conda.sh; conda init bash; conda activate snap-env; python3 main.py -config configs-snap/CNM_email.yml -mem_profiling; exec $SHELL'
-tmux new -ds snap_dblp_cnm 'source /hpi/fs00/share/complexnetSS2021/mosi/anaconda3/profile.d/conda.sh; conda init bash; conda activate snap-env; main.py -config configs-snap/CNM_dblp.yml -mem_profiling; exec $SHELL'
+tmux new -ds snap_dblp_cnm 'source /hpi/fs00/share/complexnetSS2021/mosi/anaconda3/profile.d/conda.sh; conda init bash; conda activate snap-env; python 3 main.py -config configs-snap/CNM_dblp.yml -mem_profiling; exec $SHELL'
 tmux new -ds snap_wiki_cnm 'source /hpi/fs00/share/complexnetSS2021/mosi/anaconda3/profile.d/conda.sh; conda init bash; conda activate snap-env; python3 main.py -config configs-snap/CNM_wiki.yml -mem_profiling; exec $SHELL'
